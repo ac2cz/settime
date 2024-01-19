22463fbfb425e51cc31c7ab88389567db112b91f
Chris Thompson <g0kla@arrl.net>
2024-01-19 17:53:48 -0500

    Initial commit

---
+++ settime.c
@@ -0,0 +1,32 @@
+/*
+ * settime.c
+ *
+ *  Created on: Jan 19, 2024
+ *      Author: g0kla
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/time.h>
+#include <string.h>
+
+int main(int argc, char *argv[])  {
+
+	if (argc != 2) {
+		printf("Usage: settime <unixtime>\n");
+		exit(1);
+	}
+
+	struct timeval tval;
+	time_t secs = strtol(argv[1], (char **)NULL, 10);
+
+	tval.tv_usec = 0;
+	tval.tv_sec = secs;
+	if (settimeofday(&tval, 0) != 0) {
+		printf ("error %d setting time to %ld - %s\n", errno, tval.tv_sec, strerror(errno));
+	} else {
+		printf("Setting time to %ld secs\n",secs);
+	}
+}
